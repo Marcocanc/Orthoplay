3b3d31c1b342226b5da6707c58db1575c09919cc
Marco Cancellieri <marco@cancellieri.de>
2018-01-14 15:07:33 +0100

    Initial Commit

---
+++ Orthoplay/Orthoplay.h
@@ -0,0 +1,19 @@
+//
+//  Orthoplay.h
+//  Orthoplay
+//
+//  Created by Marco Cancellieri on 14.01.18.
+//  Copyright © 2018 Marco Cancellieri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Orthoplay.
+FOUNDATION_EXPORT double OrthoplayVersionNumber;
+
+//! Project version string for Orthoplay.
+FOUNDATION_EXPORT const unsigned char OrthoplayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Orthoplay/PublicHeader.h>
+
+
